8d13992fe33f85923a6e44b0da4a8a0cb9da6193
Ventislav Yotov <>
2018-12-14 19:46:38 +0100

    refactoring

--- src/test/java/ChapterTest.java
+++ src/test/java/ChapterTest.java
@@ -9,12 +9,6 @@ import static org.mockito.Mockito.*;
 public class ChapterTest {
  private static final String CHAPTER_NAME = "Introduction";
  private static final String CHAPTER_NUMBER = "5.8";
- /*
-
-Chapters can be used in a sorted collection
-•	They must implement Comparable and override equals and hashcode.
-
-*/
 
  private Chapter chapter = mock(Chapter.class);
  private List<Chapter> CHAPTERS = new ArrayList<Chapter>();
@@ -24,12 +18,5 @@ Chapters can be used in a sorted collection
   when(chapter.getName()).thenReturn(CHAPTER_NAME);
   when(chapter.getNumber()).thenReturn(CHAPTER_NUMBER);
   //verify(chapter).
- }
- @Test
- public void verifySortedCollection(){
-  Assert.assertTrue("ss",true);
-  when(chapter.hashCode()).thenReturn(Integer.parseInt(CHAPTER_NUMBER));
-
-
  }
 }
